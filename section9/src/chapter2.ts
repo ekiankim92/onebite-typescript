c2e0b3ff8617a33e932bc60e8bf6db17c7426b45
ekim <eunkukkim1992@gmail.com>
2024-10-16 22:21:50 +0900

    inter, inference 타입 추론

--- section9/src/chapter2.ts
+++ section9/src/chapter2.ts
@@ -30,10 +30,12 @@ type E = ReturnType2<number>
  * 예제
  */
 
-type PromiseUnpack<T> = T extends 
+type PromiseUnpack<T> = T extends Promise<infer R> ? R : never
 // 1. T 는 프로미스 타입이어야한다 
 // 2. 프로미스 타입의 결과값 타입을 반환해야한다 
 
 type PromiseA = PromiseUnpack<Promise<number>>
 
 type PromiseB = PromiseUnpack<Promise<string>>
+
+type PromiseC = PromiseUnpack<Promise<() => string>>
